fea9a2e83f3aa84ec9c4c9e3d827059ffb106f6b
Marghrid <margarida@cmu.edu>
2022-12-21 18:04:03 -0500

    One source

--- js/environment.js
+++ js/environment.js
@@ -1,8 +1,10 @@
 class Environment {
 	constructor(size) {
 		this.size = size;
-		this.agent_colour = "#00805a";
-		this.background = "rgba(115, 115, 115,    0)";
+		this.agent_colour = "#fff3c7";
+		this.background_color = "#01324d";
+		this.source_color = "#40b6ff"; // #00c479
+		this.sources = []
 		this.agent_pos = [this.size / 2, this.size / 2];
 		this.agent_rot = 0;
 		this.slow_factor = 0.98;
@@ -14,6 +16,8 @@ class Environment {
 
 		// Clear current cavas:
 		ctx.clearRect(0, 0, this.size, this.size);
+		ctx.fillStyle = this.background_color;
+		ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 		// Update time:
 		const new_time = new Date().getTime();
@@ -21,7 +25,7 @@ class Environment {
 		this.update(elapsed);
 		this.last_time = new_time;
 
-
+		this.draw_source(ctx);
 		this.draw_agent(ctx);
 
 	}
@@ -40,15 +44,31 @@ class Environment {
 		let bottom_right_corner = add_pos(this.agent_pos, apply_rotation(right_bound, bottom_bound, this.agent_rot));
 
 		// Draw:
-		ctx.fillStyle = this.agent_colour;
 		ctx.beginPath();
 		ctx.moveTo(this.agent_pos[0], this.agent_pos[1]);
 		ctx.lineTo(bottom_right_corner[0], bottom_right_corner[1]);
 		ctx.lineTo(bottom_left_corner[0], bottom_left_corner[1]);
 		ctx.lineTo(top_right_corner[0], top_right_corner[1]);
 		ctx.lineTo(top_left_corner[0], top_left_corner[1]);
+		ctx.fillStyle = this.agent_colour;
 		ctx.fill();
-		ctx.fillStyle = this.background;
+		ctx.fillStyle = this.background_color;
+	}
+
+	draw_source(ctx) {
+		let source_radius = 75
+
+		let x = this.size / 2;
+		let y = this.size / 2;
+
+		let gradient = ctx.createRadialGradient(x, y, 0, x, y, source_radius);
+		gradient.addColorStop(0, this.source_color);
+		gradient.addColorStop(1, this.background_color);
+
+		ctx.arc(x, y, source_radius, 0, 2 * Math.PI);
+
+		ctx.fillStyle = gradient;
+		ctx.fill();
 	}
 
 	update(elapsed) {
